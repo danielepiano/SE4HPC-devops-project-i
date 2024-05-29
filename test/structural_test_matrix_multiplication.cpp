a4513327f13a9c569e586f81e41040355213069c
Daniele Piano <dpiano01@gmail.com>
2024-05-29 16:44:47 +0200

    ci :: add test logs on failure

--- test/structural_test_matrix_multiplication.cpp
+++ test/structural_test_matrix_multiplication.cpp
@@ -3,26 +3,16 @@
 #include <vector>
 #include <gtest/gtest.h>
 
-using IntMx = std::vector<std::vector<int>>;
-
-class IdentityScalarTestFixture :
-public ::testing::TestWithParam<int> {
-};
-INSTANTIATE_TEST_SUITE_P(
-        PositiveScalarRange,
-        IdentityScalarTestFixture,
-        testing::Range(1, 150)
-);
-INSTANTIATE_TEST_SUITE_P(
-        NegativeScalarRange,
-        IdentityScalarTestFixture,
-        testing::Range(-150, -1)
-);
-INSTANTIATE_TEST_SUITE_P(
-        ZeroScalarValue,
-        IdentityScalarTestFixture,
-        testing::Values(0)
-);
+/*
+ * #####################################################################################################################
+ * Testing A as a square matrix and B as a .
+ * Parameterized test for ranges of values:
+ *  - [-150, -1]
+ *  - 0
+ *  - [1, 150]
+ * Includes: commutative property metamorphic relation.
+ * #####################################################################################################################
+ */
 
 /*
  * #####################################################################################################################
@@ -37,18 +27,18 @@ INSTANTIATE_TEST_SUITE_P(
 TEST_P(IdentityScalarTestFixture, IdentityScalarTest) {
     int value = GetParam();
 
-    IntMx A = {
+    std::vector<std::vector<int>> A = {
             {1}
     };
-    IntMx B = {
+    std::vector<std::vector<int>> B = {
             {value}
     };
 
-    IntMx C(1, std::vector<int>(1, 0));
+    std::vector<std::vector<int>> C(1, std::vector<int>(1, 0));
 
     multiplyMatrices(A, B, C, 1, 1, 1);
 
-    IntMx expected = {
+    std::vector<std::vector<int>> expected = {
             {value}
     };
 
