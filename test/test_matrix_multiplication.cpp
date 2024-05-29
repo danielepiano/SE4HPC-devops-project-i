ef878e41621f5f06d0690289a0b73bc7b09d32bc
Daniele Piano <dpiano01@gmail.com>
2024-05-29 16:59:19 +0200

    test :: parameterized scalar x 0

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -5,36 +5,78 @@
 
 using IntMx = std::vector<std::vector<int>>;
 
-class IdentityScalarTestFixture :
+class ScalarMultiplicationTestFixture :
 public ::testing::TestWithParam<int> {
 };
 INSTANTIATE_TEST_SUITE_P(
         PositiveScalarRange,
-        IdentityScalarTestFixture,
+        ScalarMultiplicationTestFixture,
         testing::Range(1, 150)
 );
 INSTANTIATE_TEST_SUITE_P(
         NegativeScalarRange,
-        IdentityScalarTestFixture,
+        ScalarMultiplicationTestFixture,
         testing::Range(-150, -1)
 );
 INSTANTIATE_TEST_SUITE_P(
         ZeroScalarValue,
-        IdentityScalarTestFixture,
+        ScalarMultiplicationTestFixture,
         testing::Values(0)
 );
 
 /*
  * #####################################################################################################################
- * Testing neutral element property for scalar product.
- * Parameterized test for ranges of values:
- *  - [-150, -1]
- *  - 0
- *  - [1, 150]
- * Includes: commutative property metamorphic relation.
+ * WHAT
+ * - Zero-element property for scalar multiplication.
+ * HOW
+ * - Parameterized test for ranges of values:
+ *  * [-150, -1]
+ *  * 0
+ *  * [1, 150]
+ * - Exploiting commutative property.
+ * EXPECTED
+ * - Result should be 0.
  * #####################################################################################################################
  */
-TEST_P(IdentityScalarTestFixture, IdentityScalarTest) {
+TEST_P(ScalarMultiplicationTestFixture, WhenScalarX0ShouldBeZero) {
+    int value = GetParam();
+
+    IntMx A = {
+            {0}
+    };
+    IntMx B = {
+            {value}
+    };
+
+    IntMx C(1, std::vector<int>(1, 0));
+
+    multiplyMatrices(A, B, C, 1, 1, 1);
+
+    IntMx expected = {
+            {0}
+    };
+
+    ASSERT_EQ(C, expected) << "Zero-element property (left 0) for scalar multiplication failed! :(((((";
+
+    multiplyMatrices(B, A, C, 1, 1, 1);
+    ASSERT_EQ(C, expected) << "Zero-element property (right 0) for scalar multiplication failed! :(((((";
+}
+
+/*
+ * #####################################################################################################################
+ * WHAT
+ * - Neutral-element property for scalar multiplication.
+ * HOW
+ * - Parameterized test for ranges of values:
+ *  * [-150, -1]
+ *  * 0
+ *  * [1, 150]
+ * - Exploiting commutative property.
+ * EXPECTED
+ * - Result should be equal to the factor <> neutral element.
+ * #####################################################################################################################
+ */
+TEST_P(ScalarMultiplicationTestFixture, WhenScalarX1ShouldBeScalar) {
     int value = GetParam();
 
     IntMx A = {
